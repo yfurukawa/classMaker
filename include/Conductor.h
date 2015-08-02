6f869c41f203757544d0b67356ab3405861849ca
Yoshihiro Furukawa <yo4.furukawa+github@gmail.com>
2015-08-02 11:07:19 +0900

    main関数を置き換えるクラスの追加

---
+++ include/Conductor.h
@@ -0,0 +1,17 @@
+/*
+ * Conductor.h
+ *
+ *  Created on: 2015/08/02
+ *      Author: vagrant
+ */
+
+#ifndef SRC_CONDUCTOR_H_
+#define SRC_CONDUCTOR_H_
+
+class Conductor {
+public:
+	Conductor();
+	virtual ~Conductor();
+};
+
+#endif /* SRC_CONDUCTOR_H_ */
---
+++ src/Conductor.cpp
@@ -0,0 +1,18 @@
+/*
+ * Conductor.cpp
+ *
+ *  Created on: 2015/08/02
+ *      Author: vagrant
+ */
+
+#include "Conductor.h"
+
+Conductor::Conductor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Conductor::~Conductor() {
+	// TODO Auto-generated destructor stub
+}
+
